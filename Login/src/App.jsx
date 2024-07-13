2cf5384854a852a3dc6951de417f50a93329c5e4
Gourav Sharma <gourav.sharma.apsmat2932@gmail.com>
2024-07-13 22:31:16 +0530

    adding background

---
+++ Login/src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/App.css";
+
+export default function App() {
+
+  return (
+    <div id="main">
+      <div id="panel">
+        <button className="btn">Login</button>
+      </div>
+    </div>
+  )
+}
